77a164ef07852e5312ad9f44a90b6337bfac4d60
Criisss087 <cristian.barrientos@hotmail.com>
2018-12-02 23:21:13 -0300

    Rendición de comisiones terminado

--- src/PalcoNet/DAO/ItemDAO.cs
+++ src/PalcoNet/DAO/ItemDAO.cs
@@ -9,8 +9,8 @@ namespace DAO
     public class ItemDAO
     {
         public Int32 Item_ID { get; set; }
-		public Int32 Item_Monto { get; set; }
-		public Int32 Item_Cantidad { get; set; }
+		public Decimal Item_Monto { get; set; }
+		public Decimal Item_Cantidad { get; set; }
 		public String Item_Descripcion { get; set; }
 		public Int32 Factura_ID { get; set; }
         public Int32 Compra_ID { get; set; }
